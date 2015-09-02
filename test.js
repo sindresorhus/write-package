2911804eec24ea2d9ab91a4b6aa176448dab874b
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-02 15:51:39 +0700

    init

---
+++ test.js
@@ -0,0 +1,22 @@
+'use strict';
+var test = require('ava');
+var tempfile = require('tempfile');
+var readPkg = require('read-pkg');
+var fn = require('./');
+var fixture = {foo: true};
+
+test('async', function (t) {
+	var tmp = tempfile();
+	return fn(tmp, fixture).then(function () {
+		var x = readPkg.sync(tmp);
+		t.true(x.foo);
+	});
+});
+
+test('sync', function (t) {
+	var tmp = tempfile();
+	fn.sync(tmp, fixture);
+	var x = readPkg.sync(tmp);
+	t.true(x.foo);
+	t.end();
+});
