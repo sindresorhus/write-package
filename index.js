fa2f756829da63d91f7d6060e338222980689713
zkochan <zoltan.kochan@gmail.com>
2017-03-03 19:07:34 +0200

    feat: sort dependencies before saving package.json
    Sorts the `dependencies`, `devDependencies`,
    `optionalDependencies` and `peerDependencies` properties.
    
    Close #2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const path = require('path');
 const writeJsonFile = require('write-json-file');
+const sortKeys = require('sort-keys');
 
 const opts = {indent: 2};
 
@@ -11,6 +12,8 @@ module.exports = (fp, data) => {
 	}
 
 	fp = path.basename(fp) === 'package.json' ? fp : path.join(fp, 'package.json');
+
+	data = normalize(data);
 
 	return writeJsonFile(fp, data, opts);
 };
@@ -23,5 +26,22 @@ module.exports.sync = (fp, data) => {
 
 	fp = path.basename(fp) === 'package.json' ? fp : path.join(fp, 'package.json');
 
+	data = normalize(data);
+
 	writeJsonFile.sync(fp, data, opts);
 };
+
+const dependencyKeys = new Set([
+	'dependencies',
+	'devDependencies',
+	'optionalDependencies',
+	'peerDependencies'
+]);
+
+function normalize(pkg) {
+	const npkg = {};
+	for (const key of Object.keys(pkg)) {
+		npkg[key] = dependencyKeys.has(key) ? sortKeys(pkg[key]) : pkg[key];
+	}
+	return npkg;
+}
