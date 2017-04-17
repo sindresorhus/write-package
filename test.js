51a89cc6dc34bb63bbfa2fb00f59fd433a2509ee
Zoltan Kochan <zkochan@users.noreply.github.com>
2017-04-17 09:21:47 +0300

    Remove some empty properties (#5)
    The `dependencies`, `devDependencies`, `optionalDependencies` and
    `peerDependencies` properties are removed before saving if they
    are empty.
    
    Closes #4

--- test.js
+++ test.js
@@ -45,3 +45,33 @@ test('sync', t => {
 	t.deepEqual(Object.keys(x.optionalDependencies), ['bar', 'foo']);
 	t.deepEqual(Object.keys(x.peerDependencies), ['bar', 'foo']);
 });
+
+const emptyPropFixture = {
+	foo: true,
+	dependencies: {},
+	devDependencies: {},
+	optionalDependencies: {},
+	peerDependencies: {}
+};
+
+test('removes empty dependency properties', async t => {
+	const tmp = tempfile();
+	await m(tmp, emptyPropFixture);
+	const x = await readPkg(tmp, {normalize: false});
+	t.true(x.foo);
+	t.falsy(x.dependencies);
+	t.falsy(x.devDependencies);
+	t.falsy(x.optionalDependencies);
+	t.falsy(x.peerDependencies);
+});
+
+test('removes empty dependency properties sync', t => {
+	const tmp = tempfile();
+	m.sync(tmp, emptyPropFixture);
+	const x = readPkg.sync(tmp, {normalize: false});
+	t.true(x.foo);
+	t.falsy(x.dependencies);
+	t.falsy(x.devDependencies);
+	t.falsy(x.optionalDependencies);
+	t.falsy(x.peerDependencies);
+});
