fe71364b884048aefabe48f3388948e233400d89
tommy-mitchell <tmitchell7@uh.edu>
2023-10-26 20:35:58 -0500

    test(`no-path`): add `sync` tests

--- test/no-path.js
+++ test/no-path.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import esmock from 'esmock';
 import {temporaryDirectory} from 'tempy';
-import {readPackage, readPackageSync} from 'read-pkg';
+import {readPackage} from 'read-pkg';
 import {writePackage} from '../index.js';
 
 /** @type {import('./types').NoPathMacro} */
@@ -106,6 +106,69 @@ test('async - removePackageDependencies', verifyPackage, {
 	fixture: writeFixture,
 	async assertions({t, testedModule: {removePackageDependencies}, getPackageJson}) {
 		await removePackageDependencies({
+			dependencies: ['foo'],
+			devDependencies: ['bar'],
+			optionalDependencies: ['foo'],
+			peerDependencies: ['bar'],
+		});
+		const packageJson = await getPackageJson();
+
+		t.true(packageJson.foo);
+		t.deepEqual(Object.keys(packageJson.scripts), ['b', 'a']);
+		t.deepEqual(Object.keys(packageJson.dependencies), ['bar']);
+		t.deepEqual(Object.keys(packageJson.devDependencies), ['foo']);
+		t.deepEqual(Object.keys(packageJson.optionalDependencies), ['bar']);
+		t.deepEqual(Object.keys(packageJson.peerDependencies), ['foo']);
+	},
+});
+
+test('sync - writePackage', verifyPackage, {
+	async assertions({t, testedModule: {writePackageSync}, getPackageJson}) {
+		writePackageSync(writeFixture);
+		const packageJson = await getPackageJson();
+
+		t.true(packageJson.foo);
+		t.deepEqual(Object.keys(packageJson.scripts), ['b', 'a']);
+		t.deepEqual(Object.keys(packageJson.dependencies), ['bar', 'foo']);
+		t.deepEqual(Object.keys(packageJson.devDependencies), ['bar', 'foo']);
+		t.deepEqual(Object.keys(packageJson.optionalDependencies), ['bar', 'foo']);
+		t.deepEqual(Object.keys(packageJson.peerDependencies), ['bar', 'foo']);
+	},
+});
+
+test('sync - updatePackage', verifyPackage, {
+	fixture: {name: 'foo', version: '1.0.0'},
+	async assertions({t, testedModule: {updatePackageSync}, getPackageJson}) {
+		updatePackageSync({version: '2.0.0', license: 'MIT'});
+		const packageJson = await getPackageJson();
+
+		t.deepEqual(packageJson, {
+			name: 'foo',
+			version: '2.0.0',
+			license: 'MIT',
+		});
+	},
+});
+
+test('sync - addPackageDependencies', verifyPackage, {
+	fixture: writeFixture,
+	async assertions({t, testedModule: {addPackageDependenciesSync}, getPackageJson}) {
+		addPackageDependenciesSync(addFixture);
+		const packageJson = await getPackageJson();
+
+		t.true(packageJson.foo);
+		t.deepEqual(Object.keys(packageJson.scripts), ['b', 'a']);
+		t.deepEqual(Object.keys(packageJson.dependencies), ['bar', 'baz', 'foo']);
+		t.deepEqual(Object.keys(packageJson.devDependencies), ['bar', 'baz', 'foo']);
+		t.deepEqual(Object.keys(packageJson.optionalDependencies), ['bar', 'baz', 'foo']);
+		t.deepEqual(Object.keys(packageJson.peerDependencies), ['bar', 'baz', 'foo']);
+	},
+});
+
+test('sync - removePackageDependencies', verifyPackage, {
+	fixture: writeFixture,
+	async assertions({t, testedModule: {removePackageDependenciesSync}, getPackageJson}) {
+		removePackageDependenciesSync({
 			dependencies: ['foo'],
 			devDependencies: ['bar'],
 			optionalDependencies: ['foo'],
